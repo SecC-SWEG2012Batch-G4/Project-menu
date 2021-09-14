372961e76a97b64e5f204795e24689efba254555
Fatima Abayneh <88376301+Fatima-9496@users.noreply.github.com>
2021-09-14 12:19:47 -0700

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -3,72 +3,99 @@
 
 using namespace std;
 
-int menu(int &option);
+void menu();
 
 int main ()
 {
-    int opp;
+     system("color 9F");
 
-    menu(opp);
+    menu()
 
     return 0;
 }
 
 
-int menu(int &option)
-{
+void menu(){
+char opp;
 
+    cout<<"            |=============================================================================|"<<endl;
+    cout<<"            |==============This is HMSS hospital management system software===============|"<<endl;
+    cout<<"            |=================Option 1-For patient =======================================|"<<endl;
+    cout<<"            |=================Option 2-For Doctors =======================================|"<<endl;
+    cout<<"            |=================Option 3-major Information about the hospital ==============|"<<endl;
+    cout<<"            |=================Option 4-For exit ==========================================|"<<endl;
+    cout<<"            |=============================================================================|"<<endl;
 
+cout<<"\nEnter your choice from the option listed above: ";
+enter:
+cin>>opp;
 
-    cout<<"               |=============================================================================|"<<endl;
-    cout<<"               |==============This is mpdo hospital management system software===============|"<<endl;
-    cout<<"               |=================Option 1-For new patient registration=======================|"<<endl;
-    cout<<"               |=================Option 2-For existing patient===============================|"<<endl;
-    cout<<"               |=================Option 3-Information about major hospitals in Addis Ababa===|"<<endl;
-    cout<<"               |=================Option 4-For exit===========================================|"<<endl;
-    cout<<"               |=============================================================================|"<<endl;
+if(opp=='1'||opp=='2'||opp=='3'||opp=='4')
+switch(opp){
+case '1':
+    system("cls");//used to clear screan
+    patient();//patient function is being called
+    break;
 
-    cout<<endl<<endl;
+case '2':
+    system("cls");
+    doctor();//doctores function is being called
+    break;
 
-    cout<<"Enter your option: ";
+case '3':
+    system("cls");
+    hospital_information();//hospital information function is being called
+    break;
 
-    cin>>option;  //store users choice.
+case '4':
+  exit(0);
+    break;
+}
+    else
+   cout<<"\nwrong input try again: ";
+goto enter;
 
-    if (option == 1) //Option 1.
-    {
-        system("cls");
-        cout<<"he"<<endl;
-        system("pause");
-        return 0;
-    }
-
-    else if(option == 2) //Option 2.
-    {
-        system("cls");
-        cout<<"hle"<<endl;
-        system("pause");
-        return 0;
-    }
-
-    else if (option == 3) //Option 3.
-    {
-        system("cls");
-        cout<<"hleww"<<endl;
-        system("pause");
-        return 0;
-    }
-
- else if (option == 4) //Option 4.
-    {
-        system("cls");
-        return 0;
-    }
-
-    else //Value different from the options.
-    {
-        cout<<"Incorect number"<<endl;
-        system("pause");
-        return 0;
-    }
 }
 
+//patient function definition
+//patient options
+void patient(){
+int choice;
+cout<<"=================Choose your option from listed below==============";
+
+cout<<"\n\t[1].For new patient registration"<<endl;
+cout<<"\t[2].for existing patient login"<<endl;
+cout<<"\t[3].for inpatient "<<endl;
+cout<<"\t[4].For editing information"<<endl;
+cout<<"\t[5].Back main menu"<<endl;
+
+cout<<"\n\nEnter your option from listed above: ";
+cin>>choice;
+
+switch(choice){
+case 1:
+    system("cls");
+    newPatient();//calling newpatient function
+    break;
+
+case 2:
+    system("cls");
+    oldPatient();//oldpatient function is being called
+    break;
+
+ case 3:
+     system("cls");
+     inpatient();
+    break;
+
+ case 4:
+     system("cls");
+     edit();
+    break;
+case 5:
+     system("cls");
+     menu();//the system return back to menu part
+    break;
+}
+
+}
